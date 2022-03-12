acd64424d40b62191af5ee054da5eefac1b2ad82
YueYue <76854136+ItzMiracleOwO@users.noreply.github.com>
2022-03-12 10:34:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const config = require("./credentials.json")
-const runner = require("./functions/acctest.js")
-runner.login(config.email, config.pass)
+// const config = require("./credentials.json")
+// const runner = require("./functions/login.js")
+// runner.login(config.email, config.pass)
+
+module.exports = {
+    login: require("./functions/login.js").login({email, pass}),
+    getCookie: require("./functions/getCookies.js").getCookie(),
+    getHeaders: require("./functions/getHeaders.js").getHeaders(),
+}
