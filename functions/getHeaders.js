026a16479f136a2f0c41d417c6b53c67e7398be7
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2022-04-23 19:22:16 +0800

    use Promise

--- functions/getHeaders.js
+++ functions/getHeaders.js
@@ -2,53 +2,56 @@ const puppeteer = require("puppeteer-extra");
 
 const fs = require("fs");
 
-var returnValue = null;
+
 
 module.exports = {
-  getHeaders: async function (url) {
+    getHeaders: function(url) {
+        console.log("Attempting to get headers");
+        const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+        let returnValue = null;
 
-    console.log("Attempting to get headers");
+        puppeteer.use(StealthPlugin());
 
-    const StealthPlugin = require("puppeteer-extra-plugin-stealth");
-    puppeteer.use(StealthPlugin());
+        Promise(resolve => {
+            const browser = await puppeteer.launch({
+                headless: false
+            });
+            const page = await browser.newPage();
+            const navigationPromise = page.waitForNavigation();
 
-    const browser = await puppeteer.launch({
-      headless: false
-    });
-    const page = await browser.newPage();
-    const navigationPromise = page.waitForNavigation();
+            try {
+                const cookiesString = fs.readFileSync("./node_modules/ytcf/LoginCookies.json");
+                const cookies = JSON.parse(cookiesString);
+                await page.setCookie(...cookies);
 
-    try {
-      const cookiesString = await fs.readFileSync("./node_modules/ytcf/LoginCookies.json");
-      const cookies = JSON.parse(cookiesString);
-      await page.setCookie(...cookies);
+                // Opening YouTube.com
+                await page.goto(url);
 
-      // Opening YouTube.com
-      await page.goto(url);
+                await page.on("request", async request => {
+                    const requestHeaders = request.headers(); //getting headers of your request
+                    // console.log(requestHeaders)
+                    const headers = JSON.stringify(requestHeaders);
+                    if (headers.includes("x-youtube-identity-token")) {
+                        returnValue = requestHeaders;
+                        fs.writeFileSync("./node_modules/ytcf/headers.json", JSON.stringify(requestHeaders, null, 4)),
+                            function(err, res) {
+                                if (err) throw err;
+                            };
+                        resolve();
+                    }
 
-      await page.on("request", async request => {
-        const requestHeaders = request.headers(); //getting headers of your request
-        // console.log(requestHeaders)
-        const headers = JSON.stringify(requestHeaders);
-        if (headers.includes("x-youtube-identity-token")) {
-          returnValue = requestHeaders;
-          await fs.writeFileSync("./node_modules/ytcf/headers.json", JSON.stringify(requestHeaders, null, 4)),
-          function (err, res) {
-            if (err) throw err;
-          };
-        }
+                    const LoginCookies = await page.cookies();
+                    fs.writeFileSync("./node_modules/ytcf/LoginCookies.json", JSON.stringify(LoginCookies, null, 2)), //Update Login
+                        function(err) {
+                            if (err) throw err;
+                        };
+                });
 
-        const LoginCookies = await page.cookies();
-        await fs.writeFileSync("./node_modules/ytcf/LoginCookies.json", JSON.stringify(LoginCookies, null, 2)), //Update Login
-        function (err) {
-          if (err) throw err;
-        };
-      });
-      
-      return returnValue;
-      
-    } catch (e) {
-      throw new Error(e);
-    } finally {}
-  }
+                return returnValue;
+
+            } catch (e) {
+                throw new Error(e);
+            } finally {}
+        })
+    }
 };
